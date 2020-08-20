d8dcb065bc172ca8ef0aef0065e8618657b3b28e
Vinicius Domingues <vinicius.ld@outlook.com>
2020-08-19 21:48:47 -0300

    database: set up database and example model/miogration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const database = require('./database')
 const app = express()
 const port = 3000
 
 app.get('/_health_check', (req, res) => {
   return res.send({message: 'ok'})
+})
+
+app.get('/test_sequelize', async (req, res) => {
+  try {
+    await database.sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+
+    return res.send({message: 'ok'})
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+
+    return res.send({message: 'Unable to connect to the database'})
+  }
+
 })
 
 app.listen(port, () => {
